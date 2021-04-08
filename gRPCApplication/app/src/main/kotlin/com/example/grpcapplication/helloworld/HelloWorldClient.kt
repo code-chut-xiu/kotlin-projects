b6977fc290d1f641accd165363d5bbb87ad40622
Hung Le <hle@webroot.com>
2021-04-08 10:30:47 -0600

    Added gRPC Application code to the project

---
+++ gRPCApplication/app/src/main/kotlin/com/example/grpcapplication/helloworld/HelloWorldClient.kt
@@ -0,0 +1,31 @@
+package com.example.grpcapplication.helloworld
+
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+class HelloWorldClient(private val channel: ManagedChannel): Closeable {
+    private val stub: GreeterGrpcKt.GreeterCoroutineStub = GreeterGrpcKt.GreeterCoroutineStub(channel)
+
+    suspend fun greet(name: String) {
+        val request = HelloRequest.newBuilder().setName(name).build()
+        val response = stub.sayHello(request)
+        println("Received: ${response.message}")
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+}
+
+suspend fun main(args: Array<String>) {
+    val port = 50051
+    val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
+
+    val client = HelloWorldClient(channel)
+
+    val user = args.singleOrNull() ?: "world"
+    client.greet(user)
+}
+
